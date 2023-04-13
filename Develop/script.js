53503ba7aad3b6e14971827f7985aeae782e8521
Vidisha Patel <vidishapatel@yogis-mbp.lan>
2023-04-12 22:52:32 -0400

    commmit

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,64 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+const localeSettings = {};
+dayjs.locale(localeSettings);
+
+
 $(function () {
+
+  const currentHour = dayjs().format('H');
+
+  function hourlyColor() {
+    $('.time-block').each(function(){
+      const blockHour = parseInt(this.id);
+      $(this).toggleClass('past', blockHour < currentHour);
+      $(this).toggleClass('present', blockHour < currentHour);
+      $(this).toggleClass('future', blockHour < currentHour);
+    });
+  }
+
+  function textEntry () {
+    $('.saveBtn').on('click', function(){
+      const key = $(this).parent().attr('id');
+      const value = $(this).siblings('.description').val();
+      localStorage.setItem(key, value);
+    });
+  }
+
+  function refreshColor (){
+    $('.time-block').each(function(){
+      const blockHour = parseInt(this.id);
+      if (blockHour == currentHour){
+        $(this).removeClass('past future').addClass('present');
+      } else if (blockHour < currentHour){
+        $(this).removeClass('future present').addClass('past');
+      }else {
+        $(this).removeClass('past present').addClass('future');
+      }
+    });
+  }
+
+  $('.time-block').each(function(){
+    const key = $(this).attr('id');
+    const value = localStorage.getItem(key);
+    $(this).children('.description').val(value);
+  });
+
+
+  function updateTime(){
+    const dateElement = $('#date');
+    const timeElement = $('3time');
+    const currentDate = dayjs().format('dddd, MMM D,YYYY');
+    const currentTime = dayjs().format('hh:mm:ss A');
+    dateElement.text(currentDate);
+    timeElement.text(currentTime);
+  }
+
+  hourlyColor();
+  textEntry();
+  refreshColor();
+  setInterval(updateTime, 1000);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
